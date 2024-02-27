ea28e93860c23518893a7984b8d2706e72c5a864
Lauren Milechin <milechin@mit.edu>
2024-02-27 13:32:07 -0500

    fix indent

--- mujoco/run_batch_engaging.sh
+++ mujoco/run_batch_engaging.sh
@@ -1,7 +1,7 @@
-    #!/bin/bash
+#!/bin/bash
 
-    # Load the same python/anaconda module you used to install mujoco-py
-    module load python/3.8.3
+# Load the same python/anaconda module you used to install mujoco-py
+module load python/3.8.3
 
-    # Run the script
-    python mujoco_test.py
+# Run the script
+python mujoco_test.py
