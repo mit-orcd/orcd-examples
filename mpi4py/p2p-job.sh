2c7e93a88a35715915fa73da6624fdf1f9f19a1c
kcnakamu <aquachat77@gmail.com>
2024-03-17 13:14:55 -0400

    Added mpi4py

---
+++ mpi4py/p2p-job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH -N 1
+#SBATCH -n 8
+
+module load openmpi/gcc/64/1.8.1
+module load openmind/anaconda/3-2022.05
+
+mpirun -np $SLURM_NTASKS python p2p-send-recv.py
+mpirun -np $SLURM_NTASKS python p2p-array.py
