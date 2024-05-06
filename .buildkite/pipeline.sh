2ddadb90223d4d1704c942edec0141dc3e28d9ed
kcnakamu <aquachat77@gmail.com>
2024-05-06 15:31:55 -0400

    remove main branch

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -16,10 +16,3 @@ find .test/* -type f | while read -r FILE; do
   fi
 done
 
-# A deploy step only if it's the master branch
-
-if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
-  echo "  - wait"
-  echo "  - command: \"echo Deploy!\""  
-  echo "    label: \":rocket:\""
-fi
