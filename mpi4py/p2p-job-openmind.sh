0eb677863eeb3acdb932e41377133f0ea8d03088
Karen Nakamura <38799557+kcnakamu@users.noreply.github.com>
2024-05-29 12:16:17 -0400

    Update p2p-job-openmind.sh

--- mpi4py/p2p-job-openmind.sh
+++ mpi4py/p2p-job-openmind.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -l
 #SBATCH -N 1
-#SBATCH -n 4
-#SBATCH -p sched_mit_orcd
+#SBATCH -n 8
 
-module load openmpi/gcc/64/1.8.1
 module load openmind/anaconda/3-2022.05
 
 mpirun -np $SLURM_NTASKS python p2p-send-recv.py
