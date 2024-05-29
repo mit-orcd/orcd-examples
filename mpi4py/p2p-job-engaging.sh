bcd01f60492ee98d41e2f98f61d54f378047a49f
Karen Nakamura <38799557+kcnakamu@users.noreply.github.com>
2024-05-29 11:57:21 -0400

    Create p2p-job-engaging.sh

---
+++ mpi4py/p2p-job-engaging.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH -N 1
+#SBATCH -n 8
+
+module load anaconda3/2023.07
+
+source activate mpi
+mpirun -np $SLURM_NTASKS python p2p-send-recv.py
+
+source activate mpi
+mpirun -np $SLURM_NTASKS python p2p-array.py
