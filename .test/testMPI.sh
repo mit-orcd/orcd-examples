b5e48d96c88e2110253079723434462b00ea549f
kcnakamu <aquachat77@gmail.com>
2024-06-12 10:33:49 -0400

    Add mpi unlimited

--- .test/testMPI.sh
+++ .test/testMPI.sh
@@ -14,8 +14,9 @@ then
     pip install numpy mpi4py
 
     # echo "Testing Example";
-    # mpirun -np 4 python p2p-send-recv.py
-    # mpirun -np 4 python p2p-array.py
+    ulimit -l unlimited
+    mpirun -np 4 python p2p-send-recv.py
+    mpirun -np 4 python p2p-array.py
 
     echo "Testing Job";
     # sbatch p2p-job-engaging.sh
