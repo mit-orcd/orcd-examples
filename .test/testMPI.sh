4610371e16f44c25751d5ff2658d34663323171b
kcnakamu <aquachat77@gmail.com>
2024-06-12 13:29:49 -0400

    Remove semicolons

--- .test/testMPI.sh
+++ .test/testMPI.sh
@@ -3,7 +3,7 @@
 if [[ $HOSTNAME == "eofe10.mit.edu" ]];
 then
     echo $HOSTNAME
-    echo "On Engaging";
+    echo "On Engaging"
 
     cd mpi4py
     python3 -m venv .venv
@@ -18,7 +18,7 @@ then
     # mpirun -np 4 python p2p-send-recv.py
     # mpirun -np 4 python p2p-array.py
 
-    echo "Testing Job";
+    echo "Testing Job"
     # sbatch p2p-job-engaging.sh
     # tail -f
     
@@ -34,41 +34,10 @@ then
     # sacct -j $job_id -o State
     # squeue -j $job_id | wc -1
 
-elif [[ $HOSTNAME == "eofe7.mit.edu" ]];
-then
-    echo $HOSTNAME
-    echo "On Engaging (Eofe7)";
-
-    cd mpi4py
-    python3 -m venv .venv
-    source .venv/bin/activate
-    module load gcc
-    module load openmpi
-
-    pip install numpy mpi4py
-
-    # echo "Testing Example";
-    ulimit -l unlimited
-    mpirun -np 4 python p2p-send-recv.py
-    mpirun -np 4 python p2p-array.py
-
-    echo "Testing Job";
-    # sbatch p2p-job-engaging.sh
-    # tail -f
-    
-    job_id=$(sbatch -p sched_mit_orcd p2p-job-engaging.sh | awk '{print $4}')
-
-    while [[ $(squeue -j $job_id | wc -l) -gt 1 ]]; do sleep 2; done
-    pwd 
-    ls
-
-    cat slurm-$job_id.out
-    sacct -j $job_id -o State | grep -c -m 1 FAILED
-
 
 else
     echo $HOSTNAME
-    echo "Not on Engaging";
+    echo "Not on Engaging"
 
     cd mpi4py
     python3 -m venv .venv
--- .test/testMuJoCo.sh
+++ .test/testMuJoCo.sh
@@ -2,16 +2,16 @@
 
 if [[ $HOSTNAME == "eofe10.mit.edu" ]];
 then
-    echo "Testing on Engaging";
+    echo "Testing on Engaging"
     cd mujoco
     python3 -m venv .venv
     source .venv/bin/activate
     module load python/3.10.8-x86_64
     pip install 'mujoco-py<2.2,>=2.1'
-    echo "Testing Example";
+    echo "Testing Example"
 
     python mujoco_test.py
-    echo "Testing Job";
+    echo "Testing Job"
     job_id=$(sbatch run_batch_engaging.sh| awk '{print $4}')
     squeue -j $job_id
     sacct -j $job_id -o State
@@ -19,7 +19,7 @@ then
     # sbatch run_batch_engaging.sh
 
 else
-    echo "Testing on Local (Not Engaging)";
+    echo "Testing on Local (Not Engaging)"
 
     cd mujoco
     python3 -m venv .venv
