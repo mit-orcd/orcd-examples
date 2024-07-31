8511f8afc1cf39f2c63e2318d4546f78b19da404
kcnakamu <aquachat77@gmail.com>
2024-07-31 13:46:10 -0400

    Remove .snippets files (added to docs-edit repo) and updated p2p-job.sh

--- mpi4py/.snippets/p2p-engaging-setup.sh
+++
@@ -1,9 +0,0 @@
--8<-- [start:module]
-module load anaconda3/2023.07
--8<-- [end:module]
-
--8<-- [start:activate]
-conda create -n mpi
-source activate mpi
-conda install mpi4py
--8<-- [end:activate]
